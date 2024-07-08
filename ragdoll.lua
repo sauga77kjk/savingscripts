beef6d504548bbdbdeee8345bc85f6f4c9965561
sauga77kjk <169109325+sauga77kjk@users.noreply.github.com>
2024-07-08 14:30:59 +0900

    Update ragdoll.lua

--- ragdoll.lua
+++ ragdoll.lua
@@ -12,7 +12,12 @@ function impact(part,ragdoller2,chance)
 	if part then
 		part.Touched:Connect(function()
 			if math.random(1,chance)==chance then
-				ragdoller2.makesoundwithlist(part, {Volume = 10}, {8595974357, 8595983255, 8595980577, 8595975458, 8595975878})
+                local old = false
+				if old then
+ragdoller2.makesoundwithlist(part, {Volume = 10}, {8595974357, 8595983255, 8595980577, 8595975458, 8595975878})
+else
+ragdoller2.makesoundwithlist(part, {Volume = 10}, {7065398818, 7240240116, 7434241797, 7430506145})
+            end
 			end
 		end)
 	end
@@ -21,8 +26,8 @@ function asd(plr)
 	plr.CharacterAdded:Connect(function(chr)
 		chr:WaitForChild("Humanoid").Died:Connect(function()
 			local result, ragdoller2 = ragdoll(plr,25)
-			ragdoller2.makesoundwithlist(result.Head, {Volume = 10}, {528432644})
-			ragdoller2.makesoundwithlist(result.Torso, {Volume = 10}, {264486467})
+			ragdoller2.makesoundwithlist(result.Head, {Volume = 10}, {528432644, 4956861134, 7632147717, 271580013, 111124523, 5859733465, 8391420364, 1840158546, 5702637856, 28976727, 152092907})
+			ragdoller2.makesoundwithlist(result.Torso, {Volume = 10}, {264486467, 1930359546})
 			impact(result:FindFirstChild("Right Arm"),ragdoller2,52)
 			impact(result:FindFirstChild("Left Arm"),ragdoller2,52)
 			impact(result:FindFirstChild("Right Leg"),ragdoller2,52)
